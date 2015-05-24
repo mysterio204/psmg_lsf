db7686248b5aa1d92a0a76aa4b3e02a0487128a5
meikelmosby <meikel-ratz@web.de>
2015-05-24 15:10:31 +0200

    Code kommentiert von Parser-Scripts

--- Final_Project/XMLtoJSON_Parser/clearPers.js
+++ Final_Project/XMLtoJSON_Parser/clearPers.js
@@ -8,16 +8,21 @@
             xml2js = require('xml2js');
 
 
+            /* This Function clears out the JSON - Data from all not necessary information 
+               It then parses the cleared JSON-File and the RAW-JSON-File to the "writeJSON" method   */
+
+
             function clearJSON(data){
                     var clearedJSON = data.publishDetail.VVZPublish;
                     var  tree = clearedJSON[0].Einrichtungen[0].Tree;
                
             writeJSON(tree, data)
 
-             //delete data.publishDetail.global[1];
-
-        
             }
+
+            /* This Method writes both the cleared JSON-File and the RAW-JSON-File to the root-Directory 
+                When an error appears the function throws a error-message in the console
+                When no error appears as "file saved" massege appears in the console                   */
 
             function writeJSON(tree, raw){
 
@@ -28,27 +33,26 @@
                 });
 
 
-//                    fs.writeFile("PersonenDaten.json", JSON.stringify(raw), function(err){
-//                    if(err){
-//                        return console.log(err);
-//                    }
-//                });
+                    fs.writeFile("PersonenDaten.json", JSON.stringify(raw), function(err){
+                    if(err){
+                         return console.log(err);
+                   }
+               });
                 console.log("file saved");
             }
 
 
+            /* This function uses the xml2js library to parse a xml-File you declared intot a JSON file.
+                - The function parses the raw-Json-File to the function clearJSON
+                - if an error appears the console throws an error-message in the console            */
 
             function parseXMLtoJSON(){
 
                     var parser = new xml2js.Parser();
                     fs.readFile(__dirname + '/PersRaw.xml', function(err, data) {
-                        console.log(data);
                     parser.parseString(data, function (err, result) {
 
-
-                      // clearJSON(result);
-
-
+                       clearJSON(result);
 
             });
         });
--- Final_Project/XMLtoJSON_Parser/clearVV.js
+++ Final_Project/XMLtoJSON_Parser/clearVV.js
@@ -8,17 +8,22 @@
             xml2js = require('xml2js');
 
 
+             /* This Function clears out the JSON - Data from all not necessary information 
+               It then parses the cleared JSON-File  to the "writeJSON" method   */
+
+
             function clearJSON(data,i){
-                console.log(i);
+               
                       var clearedJSON = data.publishDetail.Vorlesungsverzeichnis[0].Tree;
-                //    var  tree = clearedJSON[0].Einrichtungen[0].Tree;
-            writeJSON(clearedJSON,i)
+                      writeJSON(clearedJSON,i)
 
-           console.log(clearedJSON)
+            
 
-             //delete data.publishDetail.global[1];
+              /* This Method writes the cleared JSON-File  to the root-Directory 
+                When an error appears the function throws a error-message in the console
+                When no error appears as "file saved" massege appears in the console                   */
 
-        
+
             }
 
             function writeJSON(tree,pfad){
@@ -34,60 +39,36 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+              /* This function uses the xml2js library to parse a xml-File you declared intot a JSON file.
+                - The function parses the raw-Json-File to the function clearJSON
+                - if an error appears the console throws an error-message in the console            */
+            
             function parseXMLtoJSON(n){
                
                     var parser = new xml2js.Parser();
-                    console.log(n);
                     var index = n;
                     var pfad = '/Vorlesungsverzeichnis/vv'+n+'.xml';
                   
                
                     var parsePfad = "vv"+n;
-                    
                     fs.readFile(__dirname+pfad, function(err, data) {
-                        console.log(n);
-                        console.log("i was here");
-                        console.log(__dirname+pfad);
 
                     parser.parseString(data, function (err, result) {
                        clearJSON(result,parsePfad);
 
-                        
-
-
-                    
-
-
             });
         });
                  
-                
-
-
-          
-                
-
+         
       }
 
+      /* The Method 'parseXMLtoJSON' runs as often as "Vorlesungsverzeichnis"-Files exist in the root-directory
+         The index gets parsed to the parseXMLtoJSON directory to read in the necessary file */
+
+
       for(var i = 1 ; i < 25 ; i ++ ){
-         var n = i.toString();
+      var n = i.toString();
       parseXMLtoJSON(n);
   }
-
-  
-  
-
 
 }());
