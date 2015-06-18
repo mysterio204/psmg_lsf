d767e767038eb5e7616dfa07420b3c36572067cd
mysterio204 <thiemo.kersting@googlemail.com>
2015-06-18 18:31:57 +0200

    alles neu

--- Final_Project/Server/app.js
+++ Final_Project/Server/app.js
@@ -16,6 +16,10 @@
 
     /* mensa data */
     var data = {};
+    var events= [];
+    var timesgeneral=[];
+    
+    var possibleDates=[];
 
     /* configuration */
     
@@ -281,6 +285,302 @@ var countHours = function(name,ar){
         
 
 }
+var _getAllDates = function(){
+    var types=[];
+    
+    
+    var notimecounter =0;
+    var norythmcounter =0;
+    var nodates =0;
+    var totalfaks=0;
+    var totalevents =0;
+    
+   for(var f in eventdata){
+       var timesforfak=[];
+            var name =eventdata[f].Fak;;
+                
+                var bums = JSON.parse(eventdata[f].vv)
+                var ver = jsonPath.query(bums, "$..Veranstaltung[*]");
+       
+       for(var v in ver){
+            var id = ver[v].VName;
+            var vt = ver[v].VZeit;
+           if(vt!=undefined){
+           if(vt.VZBeginDat!=undefined){var sd = vt.VZBeginDat;}
+           if(vt.VZEndDat!=undefined){var ed = vt.VZEndDat;}
+           if(vt.VZBeginn!=undefined){var td = vt.VZBeginn;}
+           }
+         
+        //console.log(id+"  "+vt);
+//           if(id!=undefined){
+           //console.log(id+"  "+sd+"  "+ed+"  "+td );
+               if(sd!=undefined&&ed!=undefined&&td!=undefined){
+              var beg= new Date( sd.replace( /(\d{2})\.(\d{2})\.(\d{4})/, "$2/$1/$3") );
+                   beg.setHours(td.slice(0,2));
+                   beg.setMinutes(td.slice(3));
+                var end= new Date( ed.replace( /(\d{2})\.(\d{2})\.(\d{4})/, "$2/$1/$3") );
+               //"VZRhythmus": "wöchentlich"
+                   
+                   if(vt!=undefined){
+                   if(vt.VZRhythmus!=undefined){
+                       var ryth = vt.VZRhythmus;
+                       if(contains(types,ryth)==false){
+                       types.push(ryth);
+                       }
+                   
+               var alle = betweenDate(new Date(beg),new Date(end),getRythm(vt.VZRhythmus));
+               if(alle.length>0){
+              
+                   for(var i in alle){
+                   timesforfak.push(alle[i]);
+                   }
+                 
+               }
+                   }else{//console.log("no rythm" );
+                        norythmcounter++;}
+               }else{//console.log("no time object");
+                    notimecounter++;}
+       
+       }else{//console.log("no start or end or time");
+            nodates++;}
+
+                
+
+        
+           totalevents++;
+//            }else{console.log("no id")}
+   }
+       var re = new RegExp(/.*Fakultät\W.*/);
+        
+    if(re.test(name)){
+      var key = name.replace(/Lehrveranstaltungen der Fakultät für /,"");;
+        
+       var fakk= {name:key,color: _getFakClass(name),data:sortAndCoutTimes(timesforfak)};
+       //fakk[key]= sortAndCoutTimes(timesforfak);
+       timesgeneral.push(fakk);
+       totalfaks++;
+           
+    
+    }
+     
+       
+   }
+   timesgeneral= fillUp(timesgeneral);
+    
+
+   //console.log("Termine konnten von "+events.length+" Veranstaltungen ermittelt werden");
+    console.log("kein rythmus angegeben: "+norythmcounter);
+    console.log("kein zeitobjekt: "+notimecounter);
+     console.log("keine zeitangaben: "+nodates);
+    console.log("fakultäten: "+ totalfaks);
+    console.log("events: "+totalevents);
+    console.log(possibleDates.length);
+    
+};
+    
+    var sortAndCoutTimes = function(arr){
+    
+    arr.sort(function(a, b) {
+    return new Date(a) - new Date(b);
+        
+});
+        
+    var secres = [];
+    var curT=new Date(arr[0]);
+    var cnt=1;
+    arr.forEach(function(obj){
+        if(contains(possibleDates,new Date(obj).getTime())==false){
+                       possibleDates.push(new Date(obj).getTime());
+                       }
+    if(new Date(obj).getTime() == new Date(curT).getTime()){
+        
+            cnt++;
+    }else{
+        
+        secres.push({x:new Date(obj).getTime()/1000,y:cnt});
+        cnt=1;
+        curT=new Date (obj);
+    
+    }
+    
+    });
+        
+    
+        return secres;
+    
+    
+    
+    };
+    
+    
+var fillUp = function(arr){
+    
+    
+    for(var f in arr){
+            for(var i in possibleDates){
+                if(hasXValue(arr[f].data,possibleDates[i])==false){
+                    var filler = {x:new Date(possibleDates[i]).getTime()/1000,y:0};
+
+                    arr[f].data.push(filler);
+           }
+        
+        }
+                arr[f].data.sort(function(a, b) {
+        return new Date(a.x) - new Date(b.x);
+        
+});
+        console.log(arr[f].data.length)
+     
+    }
+    
+//    for(var f in arr){
+//        console.log(arr[f].data.length);
+//        console.log(possibleDates.length);
+//      for (var i in possibleDates){
+//        if(hasXValue(arr[f].data,possibleDates[i])==false) {
+//                    
+//                    var filler = {x:new Date(possibleDates[i]).getTime(),y:0};
+//                    arr[f].data.push(filler);
+//            
+//        } 
+//      }
+//    }
+
+
+return arr;
+
+        
+}; 
+var contains = function (a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+};
+var hasXValue = function (a, obj) {
+    for (var i = 0; i < a.length; i++) {
+    
+        if (a[i].x == obj) {
+            
+            return true;
+        }
+    }
+ 
+    return false;
+};
+//    [ 'Blockveranstaltung',
+//  'nicht angegeben',
+//  'wöchentlich',
+//  'Blockveranstaltung + Sa',
+//  'Einzeltermin',
+//  'nach Vereinbarung',
+//  '14-tägig',
+//  'dreiwöchentlich' ]
+    
+var getRythm = function(ryth){
+switch(ryth){
+        case 'Blockveranstaltung':
+        return 1;         
+        case 'nicht angegeben':
+        return 1;
+        case 'wöchentlich':
+        return 7;
+        case 'Blockveranstaltung + Sa':
+        return 1;
+        case 'Einzeltermin':
+        return 1;
+        case 'nach Vereinbarung':
+        return 1;
+    case '14-tägig':
+        return 14;
+    case 'dreiwöchentlich':
+        return 21;
+    default:
+        return 1;
+
+}
+
+
+};    
+    
+    var _getFakClass = function (currFak){
+    switch (currFak){
+        case "Lehrveranstaltungen der Fakultät für Rechtswissenschaft":
+            return"#CDD30F"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Wirtschaftswissenschaften":
+            return"#AEA700"
+            break;
+        
+        case "Lehrveranstaltungen der Fakultät für Katholische Theologie":
+            return"#ECBC00"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Philosophie, Kunst-, Geschichts- und Gesellschaftswissenschaften":
+            return"#EC6200"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Psychologie, Pädagogik und Sportwissenschaft":
+            return"#BF002A"
+            break;
+        
+        case "Lehrveranstaltungen der Fakultät für Sprach-, Literatur- und  Kulturwissenschaften":
+            return"#9C004B"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Biologie und Vorklinische Medizin":
+            return"#4FB800"
+            break; 
+        
+        case "Lehrveranstaltungen der Fakultät für Mathematik":
+            return"#009B77"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Physik / Courses in Physics":
+            return"#008993"
+            break;
+            
+         case "Lehrveranstaltungen der Fakultät für Chemie und Pharmazie":
+            return"#0087B2"
+            break;
+            
+        case "Lehrveranstaltungen der Fakultät für Medizin":
+            return"#00556A"
+            break; 
+        
+        default:
+            return "#1D3F4B";
+    
+    }
+    
+    
+    };
+ var isDate = function(dateArg) {
+    var t = (dateArg instanceof Date) ? dateArg : (new Date(dateArg));
+    return !isNaN(t.valueOf());
+}
+ 
+ var isValidRange=function(minDate, maxDate) {
+    return (new Date(minDate) <= new Date(maxDate));
+}
+ 
+var betweenDate= function(startDt, endDt,rythm) {
+    var error = ((isDate(endDt)) && (isDate(startDt)) && isValidRange(startDt, endDt)) ? false : true;
+    var between = [];
+    if (error) console.log('error occured!!!... Please Enter Valid Dates');
+    else {
+        var currentDate = new Date(startDt),
+            end = new Date(endDt);
+        while (currentDate <= end) {
+            between.push(new Date(currentDate));
+            currentDate.setDate(currentDate.getDate() + rythm);
+        }
+    }
+    return between;
+}
         
          
     
@@ -322,7 +622,11 @@ var countHours = function(name,ar){
           
             res.send(allData);
         });
-        
+          server.use(cors());
+        server.get("/api/get/events", function (req, res) {
+          
+            res.send(timesgeneral);
+        });
         
     
     
@@ -339,7 +643,8 @@ var countHours = function(name,ar){
 
     initData();
     start();
-    setTimeout(function(){ _getDays(); }, 3000);
-    setTimeout(function(){ _getHours(); }, 3000);
+    setTimeout(function(){ _getAllDates(); }, 3000);
+    //setTimeout(function(){ _getDays(); }, 1500);
+    //setTimeout(function(){ _getHours(); }, 1500);
     
 }());
---
+++ Final_Project/lsf/chartOne.js
@@ -0,0 +1,151 @@
+
+var url = "http://localhost:3333/api/get/events"
+var jData=new Array();
+
+
+
+d3.json(url,function(data){
+
+        jData=data;
+
+var graph = new Rickshaw.Graph( {
+        element: document.getElementById("chart"),
+        width: 900,
+        height: 500,
+        renderer: 'area',
+        unstack:true,
+        stack:false,
+        stroke: false,
+        smoother:true,
+        preserve: true,
+        series: jData
+} );
+
+graph.render();
+//document.getElementById("smoothingSection").style.visibility = "hidden";
+
+
+var preview = new Rickshaw.Graph.RangeSlider( {
+        graph: graph,
+        element: document.getElementById('preview'),
+} );
+
+var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        graph: graph,
+        xFormatter: function(x) {
+                return new Date(x * 1000).toString();
+        }
+} );
+
+var annotator = new Rickshaw.Graph.Annotate( {
+        graph: graph,
+        element: document.getElementById('timeline')
+} );
+
+var legend = new Rickshaw.Graph.Legend( {
+        graph: graph,
+        element: document.getElementById('legend')
+
+} );
+
+var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
+        graph: graph,
+        legend: legend
+} );
+
+var order = new Rickshaw.Graph.Behavior.Series.Order( {
+        graph: graph,
+        legend: legend
+} );
+
+var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight( {
+        graph: graph,
+        legend: legend
+} );
+
+var smoother = new Rickshaw.Graph.Smoother( {
+        graph: graph,
+        element: document.querySelector('#smoother')
+} );
+
+
+var ticksTreatment = 'glow';
+
+var xAxis = new Rickshaw.Graph.Axis.Time( {
+        graph: graph,
+        ticksTreatment: ticksTreatment,
+        timeFixture: new Rickshaw.Fixtures.Time.Local()
+} );
+
+xAxis.render();
+
+var yAxis = new Rickshaw.Graph.Axis.Y( {
+        graph: graph,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+        ticksTreatment: ticksTreatment
+} );
+
+yAxis.render();
+
+
+// var controls = new RenderControls( {
+//         element: document.querySelector('form'),
+//         graph: graph
+// } );
+
+// add some data every so often
+
+var rendererForm = document.getElementById('renderer_form');
+
+        rendererForm.addEventListener('change', function(e) {
+                var renderMode = e.target.value;
+
+                if (renderMode == 'line') {
+                        graph.setRenderer('line');
+                } else if (renderMode == 'area') {
+                        graph.setRenderer('area');
+                } else if (renderMode == 'scatterplot') {
+                        graph.setRenderer('scatterplot');
+                }           
+                graph.render();
+
+        }, false);
+
+var messages = [
+        "Changed home page welcome message",
+        "Minified JS and CSS",
+        "Changed button color from blue to green",
+        "Refactored SQL query to use indexed columns",
+        "Added additional logging for debugging",
+        "Fixed typo",
+        "Rewrite conditional logic for clarity",
+        "Added documentation for new methods"
+];
+
+// setInterval( function() {
+//      random.removeData(seriesData);
+//      random.addData(seriesData);
+//      graph.update();
+
+// }, 3000 );
+
+// function addAnnotation(force) {
+//      if (messages.length > 0 && (force || Math.random() >= 0.95)) {
+//              annotator.add(seriesData[2][seriesData[2].length-1].x, messages.shift());
+//              annotator.update();
+//      }
+// }
+
+// addAnnotation(true);
+// setTimeout( function() { setInterval( addAnnotation, 6000 ) }, 6000 );
+
+// var previewXAxis = new Rickshaw.Graph.Axis.Time({
+//         graph: preview.previews[0],
+//         timeFixture: new Rickshaw.Fixtures.Time.Local(),
+//         ticksTreatment: ticksTreatment
+// });
+
+//previewXAxis.render();
+
+
+});
